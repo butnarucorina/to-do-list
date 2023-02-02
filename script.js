3d683bbf625c5fa119f5db08b4682a9365d35f50
Corina Butnaru <81855259+butnarucorina@users.noreply.github.com>
2023-02-02 10:00:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// alert(localStorage.getItem('todolist'));
+// addItem (localStorage.getItem('todolist'));
+
+document.querySelector("#input").addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+      const input = document.querySelector("#input");
+      addItem(input);
+    }
+      
+  });
+  
+  document.querySelector("#add_item").addEventListener("click", () => {
+    const input = document.querySelector("#input");
+    addItem(input.value);
+  });
+  // alert(localStorage.getItem('todolist'));
+  // addItem (localStorage.getItem('todolist'));
+  addItem = (input) => {
+    const item = document.createElement("div");
+    const div = document.createElement("div");
+    const checkIcon = document.createElement("i");
+    const trashIcon = document.createElement("i");
+    const text = document.createElement("p");
+  
+    item.className = "item";
+    text.textContent = input;
+  
+    checkIcon.className = "fas fa-check-square";
+    checkIcon.style.color = "lightgray";
+    checkIcon.addEventListener("click", () => {
+      checkIcon.style.color = "limegreen";
+    })
+    div.appendChild(checkIcon);
+  
+    trashIcon.className = "fas fa-trash";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click", () => {
+      item.remove();
+    })
+    div.appendChild(trashIcon);
+  
+    item.appendChild(text);
+    item.appendChild(div);
+  
+    document.querySelector("#to_do_list").appendChild(item);
+    localStorage.setItem('todolist',  document.querySelector("#input").value) ;
+    document.querySelector("#input").value = "";
+  //  localStorage.setItem('todolist', document.querySelector("#to_do_list") );
+    // alert(localStorage.getItem('todolist'));
+    
+  }
+
+  alert(localStorage.getItem('todolist'));
+  addItem (localStorage.getItem('todolist'));
+
+
+ 
